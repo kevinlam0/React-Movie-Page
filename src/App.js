5f6e460b1a5d95823fb79467a26d1e6baa539d78
kevinlam0 <kevinclam02@gmail.com>
2024-01-05 20:13:01 -0500

    Added comments for reference

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import SearchIcon from './search.svg'
 import MovieCard from "./MovieCard";
@@ -14,6 +13,12 @@ const movie1 = {
 }
 
 const App = () => {
+    // This will run at the beginning of the run because of the [] argument
+    useEffect(() => {
+        searchMovies('Kevin');
+    }, [])
+    
+    // States for changing imputs
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
 
@@ -21,24 +26,27 @@ const App = () => {
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
-
         setMovies(data.Search);
     }
-    
 
-    // This will run at the beginning of the run because of the [] argument
-    useEffect(() => {
-        searchMovies('Spiderman');
-    }, [])
+    // This shows up on the page
     return (
+        // Big div for the entire page
         <div className="app">
+            {/* Movie title */}
             <h1> Movieland </h1>
+
+            {/* Search box and search icon */}
             <div className="search">
+                {/* Text box */}
                 <input 
                     placeholder="Search for movies"
+                    // This is how the text in the box changes as you type
                     value={searchTerm}
+                    // When typed, the event calls the change to the state
                     onChange={(e) => setSearchTerm(e.target.value)}
                 />
+                {/* Click to activate the search */}
                 <img
                     src={SearchIcon}
                     alt="search"
@@ -46,19 +54,21 @@ const App = () => {
                 />
             </div>
 
-            {
-                movies?.length > 0
-                    ? (
-                    <div className="container">
-                        {movies.map((movie) => (
-                            <MovieCard movie={movie}/>
-                        ))}
+            {/* Movie cards */}
+
+            {/* Actual js */}
+            { movies?.length > 0 ? (
+                // Maps all the movies in the array with a component 
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie}/>
+                    ))}
+                </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No movies found</h2>
                     </div>
-                    ) : (
-                        <div className="empty">
-                            <h2>No movies found</h2>
-                        </div>
-                    )
+                )
             }
             
         </div>
